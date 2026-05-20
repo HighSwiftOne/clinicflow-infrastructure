e6969f1a40ea406f474c802e786ef5d0cfa8a659
Conall Keenan <56135575+HighSwiftOne@users.noreply.github.com>
2026-05-20 00:02:17 +0000

    chore: bootstrap cloud environment using local tracking parameters

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 # ==========================================
-# 1. TERRAFORM & BACKEND CONFIGURATION
+# 1. TERRAFORM INITIALIZATION CONFIGURATION
 # ==========================================
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "s3" {
-    bucket       = "clinicflow-state-vault-541495491866"
-    key          = "production/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true 
-  }
+  # TEMPORARY BOOTSTRAP OVERRIDE: 
+  # We use local execution tracking to build the bucket first, breaking the 404 loop.
+  # backend "s3" {
+  #   bucket       = "clinicflow-state-vault-541495491866"
+  #   key          = "production/terraform.tfstate"
+  #   region       = "us-east-1"
+  #   use_lockfile = true 
+  # }
 
   required_providers {
     aws = {
