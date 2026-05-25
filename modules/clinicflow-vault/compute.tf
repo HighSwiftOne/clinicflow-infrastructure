e7d9c3679be05feadb01fcbb88cd1f306c233fd0
Conall Keenan <56135575+HighSwiftOne@users.noreply.github.com>
2026-05-25 00:08:45 +0000

    fix: wire target group to application load balancer to enable ASG self-healing

--- modules/clinicflow-vault/compute.tf
+++ modules/clinicflow-vault/compute.tf
@@ -41,19 +41,14 @@ resource "aws_lb_target_group" "clinicflow_tg" {
 }
 
 # --- Listeners ---
-resource "aws_lb_listener" "http_redirect" {
+resource "aws_lb_listener" "http_forward" {
   load_balancer_arn = aws_lb.clinicflow_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.clinicflow_tg.arn
   }
 }
 
